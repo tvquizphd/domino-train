ad0fb40998f6c0bece7b0f261967f1759780785c
TVQuizPhD <tvquizphd@gmail.com>
2023-03-21 19:10:34 -0400

    domino matrix display

--- client/index.js
+++ client/index.js
@@ -1,36 +1,33 @@
 import { reactive, html } from '@arrow-js/core'
 import ArrowTags from 'arrow-tags';
 
-const and = ([...v], end='') => {
-  if (v.length < 3) return v.join(' and ');
-  const last = v.splice(v.length-1, 1) + end;
-  return [...v.map(x => `${x}, `), 'and ' + last]; 
-}
+const MIN_NODE = 1;
+const MAX_NODE = 6;
+const N_NODES = 1 + MAX_NODE - MIN_NODE;
+const toNode = index => index + MIN_NODE;
+const GRID = [...Array(N_NODES - 1).keys()].map(toNode)
 
-const toLink = (text) => {
-  const { A } = ArrowTags;
-  const aProps = {
-    id: 'id', href: (({url, search}) => url+search)
-  };
-  return A`${text}`(aProps);
-}
-
-const toCore = (link, child) => {
+const Core = (tiles) => {
   const props = {
     style: `
+      margin: 0;
       grid-row: 2;
       padding: 1rem;
       font-size: 150%;
       max-width: 300px;
+      list-style-type: none;
       font-family: sans-serif;
+      grid-template-rows: repeat(${N_NODES}, 1em);
+      grid-template-columns: repeat(${N_NODES}, 1em);
+      display: grid;
     `
   };
   const hi = 'Attention:';
-  const { Div } = ArrowTags;
-  return Div`→ ${hi} ${child} ${link} ←`(props);
+  const { Ul } = ArrowTags;
+  return Ul`${tiles}`(props);
 }
 
-const toRoot = (child) => {
+const Root = (child) => {
   const props = {
     style: `
       height: 100%;
@@ -44,26 +41,46 @@ const toRoot = (child) => {
   return Div`${child}`(props);
 }
 
+const Tile = (match, key) => {
+  const { Li } = ArrowTags;
+  const style = `
+    white-space: nowrap;
+    text-align: center;
+  `;
+  const text = ['', match[0], 'X'][match.length];
+  return Li(text)({ html, style, key });
+}
+
+const toMatrix = (edges) => {
+  const pairs = edges.map(e => new Set(e));
+  const match = x => y => x.every(y.has.bind(y));
+  const match_any = x => !!pairs.find(match(x));
+  const x_label = [[], ...GRID.map(x => [x])];
+  return x_label.concat(...GRID.map(row => {
+    return [[row+1]].concat(GRID.map(column => {
+      if (column >= row + 1) return [];
+      if (!match_any([row + 1, column])) return [];
+      return [column, row];
+    }));
+  }));
+}
+
 const toDefault = () => ({
   message: 'search "hello world"',
   url: "https://www.bing.com", 
   search: "/search?q=hello+world",
-  names: ['X', 'Y', 'Z']
+  dominos: [[1,2], [2,3], [3,4], [4,5], [5,6]]
 })
 
 const main = () => {
   const id = 'domino-display';
   const data = reactive(toDefault());
-  const { render, Ul, Li, _ } = ArrowTags;
+  const { render, Ul, _ } = ArrowTags;
   const hi = _`${d=>d.message}`;
-  const names = Ul`${d=>{
-    return and(d.names).map((str,i) => {
-      return Li(str)({html, key: i});
-    });
+  const tiles = _`${d=>{
+    return toMatrix(d.dominos).map(Tile);
   }}`;
-  const link = toLink(hi);
-  const core = toCore(link, names);
-  const root = toRoot(core);
+  const root = Root(Core(tiles));
   render(id, html, data, root);
 }
 
